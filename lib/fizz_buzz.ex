cb15aab95a3fb23c1923e21377affbcf448cf6b8
liammaricato <liammaricato@gmail.com>
2020-08-24 13:09:01 -0300

    :sparkles: Added core features

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,6 +5,25 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate
+  end
+
+  defp evaluate(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate(number), do: number
 end
